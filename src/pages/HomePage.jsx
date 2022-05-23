f2ecd1d30f443271513819d64d9c73224eca37b9
Jijooz <howardcraftera@gmail.com>
2022-05-23 16:39:35 +0500

    HeaderHome

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,9 @@
+import HeaderHome from "../components/HeaderHome/HeaderHome";
+
 const HomePage = () => {
     return (
         <div>
-            asd
+            <HeaderHome />
         </div>
     );
 }
