54a75c3cd280e470c376d9b61913d5905a9acff4
Artur Yakhin <artur.yakhin52@gmail.com>
2022-08-29 02:04:00 +0500

    asd

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
 import { Routes, Route } from 'react-router-dom';
 
-import HomePage from './pages/HomePage'
-import WorksPage from './pages/WorksPage'
+import HomePage from './pages/HomePage';
+import WorksPage from './pages/WorksPage';
 import { WorkSinglePage } from './pages/WorkSinglePage';
-import BlogPage from './pages/BlogPage'
+import BlogPage from './pages/BlogPage';
 import { BlogSinglePage } from './pages/BlogSinglePage';
-import ContactPage from './pages/ContactPage'
-import NotfoundPage from './pages/NotfoundPage'
+import ContactPage from './pages/ContactPage';
+import NotfoundPage from './pages/NotfoundPage';
 
 import Layout from './components/Layout/Layout';
 
 import './scss/app.scss';
 
 function App() {
+  const xxx = 0;
+
   return (
     <>
-    <Routes>
-      <Route path='/' element={<Layout />}>
-        <Route index element={<HomePage />} />
-        <Route path='works' element={<WorksPage />} />
-        <Route path='works/:id' element={<WorkSinglePage />} />
-        <Route path='blog' element={<BlogPage />} />
-        <Route path='blog/:id' element={<BlogSinglePage />} />
-        <Route path='contact' element={<ContactPage />} />
-        <Route path='*' element={<NotfoundPage />} />
-      </Route>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="works" element={<WorksPage />} />
+          <Route path="works/:id" element={<WorkSinglePage />} />
+          <Route path="blog" element={<BlogPage />} />
+          <Route path="blog/:id" element={<BlogSinglePage />} />
+          <Route path="contact" element={<ContactPage />} />
+          <Route path="*" element={<NotfoundPage />} />
+        </Route>
+      </Routes>
     </>
   );
 }
